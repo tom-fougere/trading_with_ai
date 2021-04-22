ef129955734fd670fdd181305e056aaec34f8da2
Tom <fougeretom@gmail.com>
2021-04-22 09:30:56 +0200

    Put parameters in argument
    add comments

--- lstm_prediction.py
+++ lstm_prediction.py
@@ -21,12 +21,30 @@ def lstm_block(input_tensor, nb_layers, units, dropout):
     return tensor
 
 
-def lstm(nb_layers, units=50, dropout=0.2):
+def lstm(num_timesteps, num_features, num_outputs=1, nb_layers=4, units=50, dropout=0.2, activation='linear'):
+    """
+    Build lstm model
+    :param num_timesteps: number of timesteps in the input
+    :param num_features: number of features in the input
+    :param num_outputs: number of outputs
+    :param nb_layers: number of layers
+    :param units: numbers of lstm neurons
+    :param dropout: fraction of the input units to drop
+    :param activation: activation function of the last (Dense) layer
+    :return:
+        - model: lstm model
+    """
 
-    # specify the input shape
-    inputs = Input(shape=(None,))
+    # Specify the input shape
+    inputs = Input(shape=(num_timesteps, num_features))
 
+    # Build the layers
     features = lstm_block(inputs, nb_layers=nb_layers, units=units, dropout=dropout)
-    outputs = Dense(units=1)(features)
 
+    # Specify the output shape
+    outputs = Dense(units=num_outputs, activation=activation)(features)
+
+    # Create the model with defined inputs and outputs
     model = Sequential(inputs=inputs, outputs=outputs)
+
+    return model
