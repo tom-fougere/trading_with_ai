666a4605b42a15e8994aca795eb017cc80df3b92
Tom <fougeretom@gmail.com>
2021-04-28 12:04:55 +0200

    Define a class for a prediction with lstm
    Integrate this lstm predictor to the script for testing several methods

--- lstm_model.py
+++ lstm_model.py
@@ -1,5 +1,8 @@
 from tensorflow.keras.layers import Input, Dense, LSTM, Dropout
 from tensorflow.keras.models import Model
+import numpy as np
+
+from predictions import ModelPrediction
 
 
 def lstm_block(input_tensor, nb_layers, units, dropout):
@@ -51,3 +54,56 @@ def lstm(num_timesteps, num_features, num_outputs=1, nb_layers=4, units=50, drop
     model = Model(inputs=inputs, outputs=outputs)
 
     return model
+
+
+class LstmPredictor(ModelPrediction):
+    def __init__(self, parameters):
+        super().__init__()
+
+        # Extract model parameters
+        self.nb_timesteps = parameters['nb_timesteps']
+        self.nb_features = parameters['nb_features']
+        nb_outputs = parameters['num_outputs'] if 'num_outputs' in parameters.keys() else 1
+        nb_layers = parameters['nb_layers'] if 'nb_layers' in parameters.keys() else 4
+        units = parameters['units'] if 'units' in parameters.keys() else 50
+        dropout = parameters['dropout'] if 'dropout' in parameters.keys() else 0.2
+        activation = parameters['activation'] if 'activation' in parameters.keys() else None
+
+        # build model
+        self.lstm = lstm(self.nb_timesteps, self.nb_features, nb_outputs, nb_layers, units, dropout, activation)
+
+        # Extract compilation parameters
+        self.optimizer = parameters['optimizer'] if 'optimizer' in parameters.keys() else 'adam'
+        self.loss = parameters['loss'] if 'loss' in parameters.keys() else 'mean_squared_error'
+        self.epochs = parameters['epochs'] if 'epochs' in parameters.keys() else 100
+        self.batch_size = parameters['batch_size'] if 'batch_size' in parameters.keys() else 32
+
+    def learn(self, data, last_index_to_learn):
+
+        # Build datasets
+        x_train = []
+        y_train = []
+        for i in range(self.nb_timesteps, len(data) - 1):
+            x_train.append(data[i - self.nb_timesteps:i])
+            y_train.append(data[i + 1])
+        x_train, y_train = np.array(x_train), np.array(y_train)
+
+        # Convert to tensor (X, Y, 1)
+        x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+
+        self.lstm.compile(optimizer=self.optimizer, loss=self.loss)
+        self.lstm.fit(x_train, y_train, epochs=self.epochs, batch_size=self.batch_size)
+
+    def predict(self, data, first_index_to_predict):
+        # Build dataset
+        x_test = []
+        for i in range(first_index_to_predict, len(data)):
+            x_test.append(data[i - self.nb_timesteps:i])
+
+        # Convert to tensor (X, Y, 1)
+        x_test = np.array(x_test)
+        x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+
+        prediction = self.lstm.predict(x_test)
+
+        return prediction
--- test_predictions.py
+++ test_predictions.py
@@ -1,8 +1,10 @@
 import yfinance as yf
 import matplotlib.pyplot as plt
+import numpy as np
 
 from predictions import *
 from arima_model import ArimaPrediction
+from lstm_model import LstmPredictor
 
 # Parameters
 first_index_to_predict = 299
@@ -11,18 +13,24 @@ first_index_to_predict = 299
 aapl = yf.Ticker('AAPL')
 history = aapl.history(period='24mo')
 stock = history['Open'].tolist()
+stock = np.divide(stock, 150.)
 
 # Models
 arima = ArimaPrediction({'p': 4, 'q': 0, 'd': 1})
+lstm = LstmPredictor({'nb_timesteps': 10, 'nb_features': 1})
 
 # Predictions
+models = [arima, lstm]
 models_prediction = ModelsPrediction(stock, last_index_for_learning=first_index_to_predict-1, expected_data=[])
-models_prediction.learn([arima])
+models_prediction.learn(models)
 models_prediction.predict()
 
 # Display data
 plt.figure()
 plt.plot(stock[first_index_to_predict:], 'o-')
-plt.plot(models_prediction.prediction[0])
+for i in range(len(models)):
+    plt.plot(models_prediction.prediction[i])
 plt.show()
+
+print('end')
 
