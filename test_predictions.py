5a3255f0a3144bc82fe4711243e993f4e8e9ab79
Tom <fougeretom@gmail.com>
2021-04-29 11:54:33 +0200

    Add preprocessing on data (minmaxscaler)

--- test_predictions.py
+++ test_predictions.py
@@ -1,6 +1,6 @@
 import yfinance as yf
 import matplotlib.pyplot as plt
-import numpy as np
+import sklearn.preprocessing as sklpp
 
 from predictions import *
 from arima_model import ArimaPrediction
@@ -14,16 +14,21 @@ first_index_to_predict = 299
 aapl = yf.Ticker('AAPL')
 history = aapl.history(period='24mo')
 aapl_stock = history['Open'].tolist()
-aapl_stock = np.divide(aapl_stock, 150.)
+aapl_stock = np.reshape(aapl_stock, (len(aapl_stock), 1))
+aapl_scaler = sklpp.MinMaxScaler()
+aapl_stock = aapl_scaler.fit_transform(aapl_stock)
 
 googl = yf.Ticker('GOOGL')
 history = googl.history(period='24mo')
 googl_stock = history['Open'].tolist()
-googl_stock = np.divide(googl_stock, 2400.)
+googl_stock = np.asarray(googl_stock)
+googl_stock = np.reshape(googl_stock, (len(googl_stock), 1))
+googl_scaler = sklpp.MinMaxScaler()
+googl_stock = googl_scaler.fit_transform(googl_stock)
 
 stocks = aapl_stock
 stocks = [aapl_stock, googl_stock]
-stocks = np.asarray(stocks).transpose()
+stocks = np.reshape(stocks, (stocks[0].shape[0], len(stocks)))
 
 # plt.figure()
 # plt.plot(aapl_stock, 'o-')
@@ -32,16 +37,21 @@ stocks = np.asarray(stocks).transpose()
 
 # Models
 arima = ArimaPrediction({'p': 4, 'q': 0, 'd': 1})
-lstm = LstmPredictor({'nb_timesteps': 10, 'nb_features': 2, 'nb_outputs': 1, 'epochs': 100})
+lstm = LstmPredictor({'nb_timesteps': 10, 'nb_features': stocks.shape[1],
+                      'nb_outputs': 1, 'epochs': 100, 'activation': 'linear'})
 delay = DelayPredictor()
 
 # Predictions
-models = [arima, lstm, delay]
+# models = [arima, lstm, delay]
 models = [lstm]
 models_prediction = ModelsPrediction(stocks, last_index_for_learning=first_index_to_predict-1)
 models_prediction.learn(models)
 models_prediction.predict()
+
+# Metrics
 kpi = models_prediction.measure_kpi(stocks[first_index_to_predict:])
+for metrics in kpi:
+    print(metrics['mse'])
 
 # Display data
 plt.figure()
